77da55dd1076a912030dc89e0de7b6c945ca5c09
anastasia.novikova <anastasija@novikova.link>
2018-05-31 16:06:25 +0300

    Test

---
+++ module1/src/test/java/test/automation/school/Test.java
@@ -0,0 +1,22 @@
+package test.automation.school;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",  "drivers/chromedriver-v2.39-win32/chromedriver.exe");
+        ChromeDriver chromeDriver = new ChromeDriver(); // new ChromeDriver() -> ctrl+atl+v
+
+        chromeDriver.get("https://www.google.com");
+     // chromeDriver.findElement(By.name("q"));
+        WebElement element = chromeDriver.findElement(By.cssSelector("[name='q']"));
+        //  chromeDriver.findElement(By.cssSelector("//*[@name='q']"));
+
+        element.sendKeys("cheese!");
+        element.sendKeys(Keys.ENTER);
+    }
+
+}
