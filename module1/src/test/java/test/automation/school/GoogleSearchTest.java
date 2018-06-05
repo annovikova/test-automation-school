2637cda6eab37bf8a9a8c9fa64675dad14cff57a
anastasia.novikova <anastasija@novikova.link>
2018-06-05 11:54:12 +0300

    Test 2

---
+++ module1/src/test/java/test/automation/school/GoogleSearchTest.java
@@ -0,0 +1,47 @@
+package test.automation.school;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class GoogleSearchTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void testName() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "../drivers/chromedriver-v2.39-win32/chromedriver.exe");
+        driver = new ChromeDriver(); // new ChromeDriver() -> ctrl+atl+v //shift+f6-> change in all places
+        driver.get("https://www.google.com");
+    }
+
+    @AfterMethod //alt+Insert -> AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void googleSearch() throws InterruptedException {
+
+        // driver.findElement(By.name("q"));
+        WebElement element = driver.findElement(By.cssSelector("[name='q']"));
+        //  driver.findElement(By.cssSelector("//*[@name='q']"));
+
+        element.sendKeys("cheese!");
+        element.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        List<WebElement> searchResultElements = driver.findElements(By.className("ajax_block_product col-xs-12 col-sm-4 col-md-3 first-in-line first-item-of-tablet-line first-item-of-mobile-line"));
+
+        assertTrue(searchResultElements.size()>0);
+    }
+}
--- module1/src/test/java/test/automation/school/tasks/LocatorsTest.java
+++ module1/src/test/java/test/automation/school/tasks/LocatorsTest.java
@@ -10,6 +10,10 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class LocatorsTest {
@@ -32,7 +36,6 @@ public class LocatorsTest {
     public void locateElementByIdTest() throws Exception {
         //TODO 1 - locate search input field by ID and assign to a variable named elementById
         WebElement elementById = driver.findElement(By.id("search_query_top"));
-
         Assert.assertTrue(elementById.isDisplayed());
         //TODO 1
     }
@@ -41,7 +44,8 @@ public class LocatorsTest {
     @Test
     public void locateElementByClassNameTest() throws Exception {
         //TODO 2 - locate *all* (also that are not visible on the screen) products by class name and put them into ArrayList Collection and check that elements list size is 14
-
+        List<WebElement> searchResultElements = driver.findElements(By.className("ajax_block_product"));
+        assertEquals(searchResultElements.size(), 14);
         //TODO 2
     }
 
@@ -49,7 +53,9 @@ public class LocatorsTest {
     @Test
     public void locateElementByTagNameTest() throws Exception {
         //TODO 3 - locate all images by tag name and put them into ArrayList Collection
-
+        ArrayList<WebElement> searchResultElements = (ArrayList<WebElement>) driver.findElements(By.tagName("img"));
+        assertEquals(searchResultElements.size(), 30);
+        //  Assert.assertTrue(searchResultElements.isDisplayed());
         //TODO 3
     }
 
@@ -57,7 +63,8 @@ public class LocatorsTest {
     @Test
     public void locateElementByNameTest() throws Exception {
         //TODO 4 - locate newsletter subscription input element by name
-
+        WebElement elementByEmail = driver.findElement(By.name("email"));
+        Assert.assertTrue(elementByEmail.isDisplayed());
         //TODO 4
     }
 
@@ -66,7 +73,8 @@ public class LocatorsTest {
     public void locateElementByLinkTextTest() throws Exception {
         //TODO 5 - locate "T-Shirts" element by link text
         // NOTE - linkText() is case sensitive and should be selected as it is rendered on the screen, not in HTML
-
+        WebElement elementByLinkText = driver.findElement(By.linkText("T-shirts"));
+        Assert.assertTrue(elementByLinkText.isDisplayed());
         //TODO 5
     }
 
@@ -74,7 +82,8 @@ public class LocatorsTest {
     @Test
     public void locateElementByCssSelctorTest() throws Exception {
         //TODO 6 - locate only *visible* products element by CSS and put them into ArrayList Collection and check that elements list size is 7
-
+        List<WebElement> searchResultElements = driver.findElements(By.cssSelector("#homefeatured.active .ajax_block_product"));
+        assertEquals(searchResultElements.size(), 7);
         //TODO 6
 
     }
@@ -85,7 +94,8 @@ public class LocatorsTest {
         //NOTE - you have to indicate all class names that are specified on the current element
         // This is contrary to CSS Selector where you can specify just one of the class names
         // TIP - use 'contains()'
-
+        List<WebElement> searchResultElements = driver.findElements(By.xpath("//*[@id='homefeatured'][contains(@class, 'active')]/*[contains(@class, 'ajax_block_product')]"));
+        Assert.assertTrue(searchResultElements.size() == 7);
         //TODO 7
     }
 
