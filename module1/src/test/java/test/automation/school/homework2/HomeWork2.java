071d039262b4f0196318dadeaf8f7a923fcd530e
anastasia.novikova <anastasija@novikova.link>
2018-06-06 16:54:04 +0300

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	module1/src/test/java/test/automation/school/tasks/LocatorsTest.java

--- module1/src/test/java/test/automation/school/homework2/HomeWork2.java
+++ module1/src/test/java/test/automation/school/homework2/HomeWork2.java
@@ -6,13 +6,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWork2 {
     private WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() throws Exception {
         System.setProperty("webdriver.firefox.bin", "C:\\Users\\anastasia.kulakova\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
         System.setProperty("webdriver.gecko.driver", "..\\drivers\\geckodriver-v0.20.1-win64\\geckodriver.exe");
@@ -27,9 +29,19 @@ public class HomeWork2 {
 //    }
 
     @Test
+    public void checkTitle() throws Exception {
+        String getTitle = driver.getTitle();
+        System.out.println("Page title Is : " + getTitle);
+        Assert.assertEquals("Super Calculator", getTitle);
+    }
+
+    @Test
     public void checkSum() throws Exception {
         WebElement inptFirstNumber = driver.findElement(By.cssSelector("input[ng-model='first']"));
         inptFirstNumber.sendKeys("1");
+        WebElement dropdown = driver.findElement(By.cssSelector("select.span1"));
+        Select selectDropdown = new Select(dropdown);
+        selectDropdown.selectByVisibleText("+");
         WebElement inptSecondNumber = driver.findElement(By.cssSelector("input[ng-model='second']"));
         inptSecondNumber.sendKeys("2");
         WebElement goButton = driver.findElement(By.cssSelector("#gobutton"));
@@ -40,34 +52,23 @@ public class HomeWork2 {
     }
 
     @Test
-    public void checkSubtraction() throws Exception {
-        WebElement inptFirstNumber = driver.findElement(By.cssSelector("input[ng-model='first']"));
-        inptFirstNumber.sendKeys("1");
-        WebElement dropdown = driver.findElement(By.cssSelector("select.span1"));
-        Select selectDropdown = new Select(dropdown);
-        selectDropdown.selectByVisibleText("-");
-        WebElement inptSecondNumber = driver.findElement(By.cssSelector("input[ng-model='second']"));
-        inptSecondNumber.sendKeys("2");
-        WebElement goButton = driver.findElement(By.cssSelector("#gobutton"));
-        goButton.click();
+    public void checkNumberOfResults() throws Exception {
+        for (int i = 1; i < 4; i++) {
+            WebElement inptFirstNumber = driver.findElement(By.cssSelector("input[ng-model='first']"));
+            inptFirstNumber.sendKeys("1");
+            WebElement dropdown = driver.findElement(By.cssSelector("select.span1"));
+            Select selectDropdown = new Select(dropdown);
+            selectDropdown.selectByVisibleText("+");
+            WebElement inptSecondNumber = driver.findElement(By.cssSelector("input[ng-model='second']"));
+            inptSecondNumber.sendKeys("2");
+            WebElement goButton = driver.findElement(By.cssSelector("#gobutton"));
+            goButton.click();
+            Thread.sleep(5000);
+        }
         Thread.sleep(5000);
-        String result = driver.findElement(By.cssSelector("h2.ng-binding")).getText();
-        Assert.assertEquals(result, "-1");
+        List<WebElement> resultList = driver.findElements(By.cssSelector("tr.ng-scope"));
+        Assert.assertTrue(resultList.size() == 3);
     }
 
-    @Test
-    public void locateElementByIdTest4() throws Exception {
-        WebElement inptFirstNumber = driver.findElement(By.cssSelector("input[ng-model='first']"));
-        inptFirstNumber.sendKeys("1");
-        WebElement dropdown = driver.findElement(By.cssSelector("select.span1"));
-        Select selectDropdown = new Select(dropdown);
-        selectDropdown.selectByVisibleText("/");
-        WebElement inptSecondNumber = driver.findElement(By.cssSelector("input[ng-model='second']"));
-        inptSecondNumber.sendKeys("2");
-        WebElement goButton = driver.findElement(By.cssSelector("#gobutton"));
-        goButton.click();
-        Thread.sleep(5000);
-        String result = driver.findElement(By.cssSelector("h2.ng-binding")).getText();
-        Assert.assertEquals(result, "0.5");
-    }
+
 }
